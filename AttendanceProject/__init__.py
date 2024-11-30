962a08a68671798f8045746e203bb0e3aabb4b11
mohammad <81656341+Moha-hub-geldroid@users.noreply.github.com>
2024-11-30 15:15:10 +0300

    Update __init__.py

--- AttendanceProject/__init__.py
+++ AttendanceProject/__init__.py
@@ -11,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 db = SQLAlchemy(app)
-mail = Mail(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = 'info'
